e2f9b309095da057e9dc0bdba5b50993cb739bc8
jacoblontoc <dizzymango@icloud.com>
2025-03-18 23:56:36 -0400

    oh-my-posh added

---
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+#Aliases
+Set-Alias tt tree
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias vim nvim
+
+#Prompt
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\Jacob L\Documents\PowerShell\jandedobbeleer.omp.json' | Invoke-Expression
+
+
+#Terminal Icons
+Import-Module Terminal-Icons
